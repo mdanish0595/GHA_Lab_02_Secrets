4c2f7895694c84e444e3759c5b6a71a0347c41d6
Danish <737615@cognizant.com>
2025-10-05 11:59:30 +0100

    added backend config in action file

--- terraform-sg/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "dan-terraform-01"
-    key            = "tf-statefile/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-  }
-}
